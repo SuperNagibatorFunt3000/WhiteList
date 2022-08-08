e10036cc127f5fa9568961f05fe6d910242d8986
SuperNagibatorFunt3000 <110674014+SuperNagibatorFunt3000@users.noreply.github.com>
2022-08-08 11:00:56 +0300

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,7 @@
+local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/SuperNagibatorFunt3000/White-List/main/WhiteListSettings", true))()
+
+if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
+  print("yellow blue vase") -- Ваш скрипт
+else
+  game:service('Players').LocalPlayer:Kick('Bruh')
+end
